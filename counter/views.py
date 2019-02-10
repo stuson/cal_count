bcfe72f28618172cf6741bd734b0c445976e76e5
unknown <sam.tuson@hotmail.co.uk>
2019-02-10 20:30:50 +0000

    Add food detail view

--- counter/views.py
+++ counter/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.models import User
 from .models import (
     Food,
@@ -7,12 +7,15 @@ from .models import (
     FoodTag
 )
 
-class RecentDaysView(ListView):
+class MyFoodInstancesListView(ListView):
     model = FoodInstance
     context_object_name = 'foodInstances'
     paginate_by = 5
     template_name = 'counter/home.html'
-    
 
     def get_queryset(self):
         return FoodInstance.objects.filter(user=self.request.user).order_by('-date')
+
+class FoodDetailView(DetailView):
+    model = Food
+    template_name = 'counter/food_detail.html'
