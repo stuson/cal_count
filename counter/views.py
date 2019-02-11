a2cd043f2636abd7d32fc5933af3f511746e27a4
unknown <sam.tuson@hotmail.co.uk>
2019-02-11 00:04:33 +0000

    Group by date in views and sum totals for day

--- counter/views.py
+++ counter/views.py
@@ -1,6 +1,8 @@
+from pandas import DataFrame
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.models import User
+from django.db.models import Sum
 from .models import (
     Food,
     FoodInstance,
@@ -9,12 +11,23 @@ from .models import (
 
 class MyFoodInstancesListView(ListView):
     model = FoodInstance
-    context_object_name = 'foodInstances'
-    paginate_by = 5
+    context_object_name = 'dates'
+    # paginate_by = 5
     template_name = 'counter/home.html'
 
     def get_queryset(self):
-        return FoodInstance.objects.filter(user=self.request.user).order_by('-date')
+        user_instances = FoodInstance.objects.filter(user=self.request.user)
+        dates = user_instances.values_list('date', flat=True).distinct()
+        grouped_list = []
+        for date in dates:
+            today = user_instances.filter(user=self.request.user, date=date)
+            grouped_list.append({
+                'date': date,
+                'foodInstances': today,
+                'day_totals': DataFrame([x.totals for x in today]).sum().to_dict()
+            })
+
+        return grouped_list
 
 class FoodDetailView(DetailView):
     model = Food
